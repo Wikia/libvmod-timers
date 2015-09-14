23bb06634bb1155835df2d3ff2b15628d64c3270
Artur Sitarski <artur.sitarski@wikia-inc.com>
2015-09-14 10:12:47 +0000

    Wikia changes - implementation of req_response_time function

--- src/vmod_timers.c
+++ src/vmod_timers.c
@@ -10,3 +10,8 @@ VCL_TIME vmod_req_start(const struct vrt_ctx *ctx) {
 VCL_INT vmod_duration2int(const struct vrt_ctx *ctx, VCL_DURATION dur, VCL_INT mult) {
 	return dur * mult;
 }
+
+// Returns time in miliseconds from delegation to backend to first byte from backend
+VCL_INT vmod_req_response_time(const struct vrt_ctx *ctx) {
+  return (int) ((ctx->req->t_prev - ctx->req->t_first) * 1000);
+}
